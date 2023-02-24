5af25301fa52fcdcfb6d03aa29ffb072e61d64aa
msxrougne <mathilde.serougne@ensae.fr>
2023-02-24 17:14:22 +0000

    bref

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -1,50 +1,25 @@
-class Graph:
-    """
-    A class representing graphs as adjacency lists and implementing various algorithms on the graphs. Graphs in the class are not oriented. 
-    Attributes: 
-    -----------
-    nodes: NodeType
-        A list of nodes. Nodes can be of any immutable type, e.g., integer, float, or string.
-        We will usually use a list of integers 1, ..., n.
-    graph: dict
-        A dictionnary that contains the adjacency list of each node in the form
-        graph[node] = [(neighbor1, p1, d1), (neighbor1, p1, d1), ...]
-        where p1 is the minimal power on the edge (node, neighbor1) and d1 is the distance on the edge
-    nb_nodes: int
-        The number of nodes.
-    nb_edges: int
-        The number of edges. 
-    """
 
+class Graph:
     def __init__(self, nodes=[]):
-        """
-        Initializes the graph with a set of nodes, and no edges. 
-        Parameters: 
-        -----------
-        nodes: list, optional
-            A list of nodes. Default is empty.
-        """
         self.nodes = nodes
         self.graph = dict([(n, []) for n in nodes])
         self.nb_nodes = len(nodes)
         self.nb_edges = 0
-    
 
     def __str__(self):
         """Prints the graph as a list of neighbors for each node (one per line)"""
         if not self.graph:
-            output = "The graph is empty"            
+            output = "The graph is empty"          
         else:
             output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
             for source, destination in self.graph.items():
                 output += f"{source}-->{destination}\n"
         return output
-    
+
     def add_edge(self, node1, node2, power_min, dist=1):
         """
         Adds an edge to the graph. Graphs are not oriented, hence an edge is added to the adjacency list of both end nodes. 
-
-        Parameters: 
+        Parameters:
         -----------
         node1: NodeType
             First end (node) of the edge
@@ -54,28 +29,93 @@ class Graph:
             Minimum power on this edge
         dist: numeric (int or float), optional
             Distance between node1 and node2 on the edge. Default is 1.
+        la complexité est en O(V!) en effet V! est l'ensemble des chemins
+        possibles dans le pire des cas l'algorithme parcours tous les chemins
+        possibles
         """
-        if node1 not in self.graph:
-            self.graph[node1] = []
-            self.nb_nodes += 1
-            self.nodes.append(node1)
-        if node2 not in self.graph:
-            self.graph[node2] = []
-            self.nb_nodes += 1
-            self.nodes.append(node2)
-
-        self.graph[node1].append((node2, power_min, dist))
-        self.graph[node2].append((node1, power_min, dist))
+        self.graph[node1].append([node2, power_min, dist])
+        self.graph[node2].append([node1, power_min, dist])
         self.nb_edges += 1
-    
 
     def get_path_with_power(self, src, dest, power):
-        raise NotImplementedError
-    
+        
+
+        def min_dist(trajets_possibles):
+            if trajets_possibles==[]:
+                return None
+            distances=[]
+            for i in range(len(trajets_possibles)):
+                s=0
+                for j in range(len(trajets_possibles[i])-1) :
+                    for k in self.graph[trajets_possibles[i][j]] :
+                        if k[0]==trajets_possibles[i][j+1] :
+                            s+=k[2] 
+                distances.append(s) #liste avec toutes les distances, indexées comme trajets_possibles, mtn on cherche l'indice de la distance la plus petite
+            indice_min=[0,distances[0]]
+            for i in range(1,len(distances)) :
+                if indice_min[1] > distances[i] :
+                    indice_min=[i,distances[i]]
+            return trajets_possibles[i]
+
+
+     
+
+        trajets_possibles=[]
+
+        def rec(start, parcourus,chemin): #to be continued
+            print(chemin)
+            for i in self.graph[start]:
+                if i[1] > power :
+                    continue #skip ceux nécessitant une trop grande puissance par rapport à notre power
+                elif parcourus[i[0]] :
+                    continue #skip ceux déjà parcourus
+                elif i[0] == dest:
+                    parcourus[start]=True
+                    chemin.append(start)
+                    chemin.append(dest)
+                    trajets_possibles.append(chemin)    
+                else:
+                    parcourus[start]=True
+                    chemin.append(start)
+                    rec(i[0], parcourus, chemin)
+
+        parcourus=[False]*self.nb_nodes
+        parcourus[src]=True
+        rec(src, parcourus, [])
+        print(trajets_possibles)
+        return min_dist(trajets_possibles)
+        
 
     def connected_components(self):
-        raise NotImplementedError
+        '''
+        La complexité est en O(V+E) car chaques sommets et chaques arêtes sont parcourus au plus une fois
+        '''
+        deja_vu = []    # liste les sommets dejà vu
+        connected_components = []   # liste de liste des composantes connectées
 
+        def parcours_graphe(q):
+            '''
+            fonction récursive qui parcours une composante connectée du graphe
+            '''
+            if q==[] :
+                return
+            
+            s = q.pop()[0]
+            if s in deja_vu:
+                parcours_graphe(q)
+            else:
+                deja_vu.append(s)
+                connected_components[-1].append(s)
+                q += self.graph[s]
+                parcours_graphe(q)
+
+        for i in self.nodes:
+            if i in deja_vu:
+                continue
+            else:
+                connected_components.append([])
+                parcours_graphe(self.graph[i])
+        return connected_components
 
     def connected_components_set(self):
         """
@@ -83,10 +123,10 @@ class Graph:
         For instance, for network01.in: {frozenset({1, 2, 3}), frozenset({4, 5, 6, 7})}
         """
         return set(map(frozenset, self.connected_components()))
-    
+
     def min_power(self, src, dest):
         """
-        Should return path, min_power. 
+        Should return path, min_power.
         """
         raise NotImplementedError
 
@@ -94,34 +134,31 @@ class Graph:
 def graph_from_file(filename):
     """
     Reads a text file and returns the graph as an object of the Graph class.
-
-    The file should have the following format: 
+    The file should have the following format:
         The first line of the file is 'n m'
-        The next m lines have 'node1 node2 power_min dist' or 'node1 node2 power_min' (if dist is missing, it will be set to 1 by default)
+        The next m lines have 'node1 node2 power_min dist' or 'node1 node2 power_min'
+        (if dist is missing, it will be set to 1 by default)
         The nodes (node1, node2) should be named 1..n
         All values are integers.
-
-    Parameters: 
+    Parameters:
     -----------
     filename: str
         The name of the file
-
-    Outputs: 
+    Outputs:
     -----------
-    g: Graph
+    G: Graph
         An object of the class Graph with the graph from file_name.
     """
-    with open(filename, "r") as file:
-        n, m = map(int, file.readline().split())
-        g = Graph(range(1, n+1))
-        for _ in range(m):
-            edge = list(map(int, file.readline().split()))
-            if len(edge) == 3:
-                node1, node2, power_min = edge
-                g.add_edge(node1, node2, power_min) # will add dist=1 by default
-            elif len(edge) == 4:
-                node1, node2, power_min, dist = edge
-                g.add_edge(node1, node2, power_min, dist)
+    files = open(filename, "r")
+    for ligne in files:
+        ligne = ligne.split(" ")
+        if len(ligne) == 2:
+            graph = Graph([i+1 for i in range(int(ligne[0]))])
+        else:
+            if len(ligne) == 4:
+                graph.add_edge(int(ligne[0]), int(ligne[1]), int(ligne[2]), int(ligne[3]))
             else:
-                raise Exception("Format incorrect")
-    return g
+                graph.add_edge(int(ligne[0]), int(ligne[1]), int(ligne[2]))
+    return graph
+
+
